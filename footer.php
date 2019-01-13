204e0bbfd0d50a235f8e8ef666bcb831606fd67e
Oleksandr Klochko <utoyvo@protonmail.com>
2019-01-13 06:00:33 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -15,6 +15,19 @@
 	<footer id="colophon" class="site-footer">
 		<?php get_sidebar( 'content-bottom' ); ?>
 
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+			<nav class="social-navigation container" role="navigation">
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'social',
+					'menu_class'     => 'social-links-menu',
+					'depth'          => 1,
+					'link_before'    => '<span class="screen-reader-text">',
+					'link_after'     => '</span>',
+				) ); ?>
+			</nav><!-- .social-navigation -->
+		<?php endif; ?>
+
 		<div class="site-info container">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-xyz' ) ); ?>" target="_blank">
 				<?php
@@ -27,6 +40,8 @@
 			printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-xyz' ), 'wp-xyz', '<a href="http://utoyvo.xyz">utoyvo</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+
+	<div id="up-down"><i class="fas fa-angle-up"></i></div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
