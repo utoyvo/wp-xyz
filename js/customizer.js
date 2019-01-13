a7e8930a51d13d7d4930d7f7c2cf6ac7b2fb4343
Oleksandr Klochko <utoyvo@protonmail.com>
2019-01-13 03:44:42 +0200

    Create customizer.js

---
+++ js/customizer.js
@@ -0,0 +1,36 @@
+/**
+ * File customizer.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+( function( $ ) {
+
+	// Site title.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title' ).css( {
+					'clip': 'auto',
+					'position': 'relative'
+				} );
+				$( '.site-title a' ).css( {
+					'color': to
+				} );
+			}
+		} );
+	} );
+} )( jQuery );
