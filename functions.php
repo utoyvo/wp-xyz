f3d09cbf7e320c3e31f96f9fa7286dbe963c6b6a
Oleksandr Klochko <utoyvo@protonmail.com>
2019-01-13 04:54:07 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -176,3 +176,23 @@ function wp_xyz_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wp_xyz_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
